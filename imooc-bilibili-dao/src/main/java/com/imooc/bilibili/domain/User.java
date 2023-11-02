5dd1dcfc7a5de7d6cfdfa1588d209048c4733751
Coderxia Luo <guojingzhou@huayun.com>
2023-11-02 12:44:51 +0800

    feat: 用户第一个接口 - 获取公钥的

---
+++ imooc-bilibili-dao/src/main/java/com/imooc/bilibili/domain/User.java
@@ -0,0 +1,71 @@
+package com.imooc.bilibili.domain;
+
+import java.util.Date;
+
+// 对应数据库表，有点类似 mybatis-plus 自动生成的，现在手写
+// Alt + Insert 快速生成 get 和 set
+public class User {
+       public Long id;
+       public String phone;
+       public String email;
+       public String password;
+       public String salt;
+       public Date createTime;
+       public Date updateTime;
+
+       public Long getId() {
+              return id;
+       }
+
+       public void setId(Long id) {
+              this.id = id;
+       }
+
+       public String getPhone() {
+              return phone;
+       }
+
+       public void setPhone(String phone) {
+              this.phone = phone;
+       }
+
+       public String getEmail() {
+              return email;
+       }
+
+       public void setEmail(String email) {
+              this.email = email;
+       }
+
+       public String getPassword() {
+              return password;
+       }
+
+       public void setPassword(String password) {
+              this.password = password;
+       }
+
+       public String getSalt() {
+              return salt;
+       }
+
+       public void setSalt(String salt) {
+              this.salt = salt;
+       }
+
+       public Date getCreateTime() {
+              return createTime;
+       }
+
+       public void setCreateTime(Date createTime) {
+              this.createTime = createTime;
+       }
+
+       public Date getUpdateTime() {
+              return updateTime;
+       }
+
+       public void setUpdateTime(Date updateTime) {
+              this.updateTime = updateTime;
+       }
+}
---
+++ imooc-bilibili-dao/src/main/java/com/imooc/bilibili/domain/UserInfo.java
@@ -0,0 +1,87 @@
+package com.imooc.bilibili.domain;
+
+import java.util.Date;
+
+public class UserInfo {
+       public Long id;
+       public Long userId;
+       public String nick;
+       public String avatar;
+       public String sign;
+       public String gender;
+       public String birth;
+       public Date createTime;
+       public Date updateTime;
+
+       public Long getId() {
+              return id;
+       }
+
+       public void setId(Long id) {
+              this.id = id;
+       }
+
+       public Long getUserId() {
+              return userId;
+       }
+
+       public void setUserId(Long userId) {
+              this.userId = userId;
+       }
+
+       public String getNick() {
+              return nick;
+       }
+
+       public void setNick(String nick) {
+              this.nick = nick;
+       }
+
+       public String getAvatar() {
+              return avatar;
+       }
+
+       public void setAvatar(String avatar) {
+              this.avatar = avatar;
+       }
+
+       public String getSign() {
+              return sign;
+       }
+
+       public void setSign(String sign) {
+              this.sign = sign;
+       }
+
+       public String getGender() {
+              return gender;
+       }
+
+       public void setGender(String gender) {
+              this.gender = gender;
+       }
+
+       public String getBirth() {
+              return birth;
+       }
+
+       public void setBirth(String birth) {
+              this.birth = birth;
+       }
+
+       public Date getCreateTime() {
+              return createTime;
+       }
+
+       public void setCreateTime(Date createTime) {
+              this.createTime = createTime;
+       }
+
+       public Date getUpdateTime() {
+              return updateTime;
+       }
+
+       public void setUpdateTime(Date updateTime) {
+              this.updateTime = updateTime;
+       }
+}
