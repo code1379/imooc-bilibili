c7ebd8aa16ec85d64b114eb9ef59819e2e5f364d
Coderxia Luo <guojingzhou@huayun.com>
2023-11-03 11:26:31 +0800

    feat: 用户关注分组表，实体类 -> Service -> Dao -> Mapper

---
+++ imooc-bilibili-dao/src/main/java/com/imooc/bilibili/domain/FollowingGroup.java
@@ -0,0 +1,60 @@
+package com.imooc.bilibili.domain;
+
+import java.util.Date;
+
+public class FollowingGroup {
+    public Long id;
+    public Long userId;
+    public String name;
+    public String type;
+    public Date createTime;
+    public Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ imooc-bilibili-dao/src/main/java/com/imooc/bilibili/domain/UserFollowing.java
@@ -0,0 +1,53 @@
+package com.imooc.bilibili.domain;
+
+import java.util.Date;
+
+
+//  实体类 -> Dao -> Service -> Api
+public class UserFollowing {
+    public Long id;
+    public Long userId;
+    public Long followingId;
+    public Long groupId;
+    public Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getFollowingId() {
+        return followingId;
+    }
+
+    public void setFollowingId(Long followingId) {
+        this.followingId = followingId;
+    }
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
